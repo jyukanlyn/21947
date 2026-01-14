b746a7a40621336a02a97fa884fd35182175e6c2
jyukanlyn <jyukanlyn@gmail.com>
2026-01-14 16:11:30 +1030

    Update characters.js

--- SPLL_E110040/data/characters.js
+++ SPLL_E110040/data/characters.js
@@ -1,15 +1,15 @@
 export const characters = {
   "二羽 一葉": {
-    side: "left";
-    nameColor: "#FF5809";
+    side: "left",
+    nameColor: "#FF5809",
     sprites: {
       normal: "assets/char/21.png"
     }
   },
   "久世　傾": {
     side: "right",
-    nameColor: "#E6CAFF";
-    color: black;
+    nameColor: "#E6CAFF",
+    color: "black",
     sprites: {
       normal: "assets/char/947.png"
     }
